bae7afc10b733a2f6608f1bf93d403182237791b
haruyama.makoto <makoto.haruyama@dena.com>
2020-08-03 16:04:47 +0900

    reverse and swap

--- tips/src/main/java/Tips.java
+++ tips/src/main/java/Tips.java
@@ -1,4 +1,13 @@
 public class Tips {
+  public static void reverse(int[] nums, int start) {
+    int i = start, j = nums.length - 1;
+    while(i < j) {
+      swap(nums, i, j);
+      i++;
+      j--;
+    }
+  }
+
   public static void swap(int[] nums, int i, int j) {
     int temp = nums[i];
     nums[i] = nums[j];
--- tips/src/test/java/TipsTest.java
+++ tips/src/test/java/TipsTest.java
@@ -5,6 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class TipsTest {
 
   @Test
+  void reverse() {
+    int[] target = new int[] {1, 3, 2};
+    Tips.reverse(target, 0);
+    assertArrayEquals(target, new int[] {2, 3, 1});
+  }
+
+  @Test
   void swap() {
     int[] target = new int[] {1, 2, 3};
     Tips.swap(target, 1, 2);
